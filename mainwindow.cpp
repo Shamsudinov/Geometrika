2e2133375bb62f34c528acf839f6ea9d72bda256
Shamsudin <shamsudinov93@bk.ru>
2023-12-13 15:16:07 +0300

    Разработка
    Добавил виджет QGraphicsView и сцену QGraphicsScene для отрисовки.
    Размер главного окна временно фиксированный.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,31 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+//    this->resize(500,500);          // Задаем размеры виджета, то есть окна
+    // Фиксируем размеры виджета
+    this->setFixedSize(520,650);
+
+    // Инициализируем графическую сцену
+    scene = new GraphicsScene();
+
+    // Устанавливаем графическую сцену в graphicsView
+    ui->graphicsView->setScene(scene);
+    // Устанавливаем сглаживание
+    ui->graphicsView->setRenderHint(QPainter::Antialiasing);
+    // Отключаем скроллбар по вертикали
+    ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    // Отключаем скроллбар по горизонтали
+    ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    // Задаем размеры graphicsView
+    ui->graphicsView->resize(500,500);
+    // Устанавливаем область графической сцены
+    scene->setSceneRect(-250,-250,500,500);
+
+    // Добавляем горизонтальную линию через центр
+    scene->addLine(-250,0,250,0,QPen(Qt::black));
+    // Добавляем вертикальную линию через центр
+    scene->addLine(0,-250,0,250,QPen(Qt::black));
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <graphicsscene.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    GraphicsScene  *scene;
 };
 #endif // MAINWINDOW_H
